d41b8747291720a96bf9cc3d805a5c33c4b51f24
Денис Джабаров <denis.smit72@gmail.com>
2023-09-28 14:23:38 +0500

    mvp-12 - теперь у меня крутой edit book, но пока что ничего не работает :с (по идее)

---
+++ src/entities/book/model/$$book/core.ts
@@ -0,0 +1,33 @@
+import { combine, createEffect, createEvent, createStore, sample } from 'effector'
+
+import { Book } from 'shared/api'
+
+//* Stores
+export const $books = createStore<Book[]>([])
+export const $activeBookId = createStore<number | null>(null)
+export const $book = combine($books, $activeBookId, (books, activeBookId) => {
+  if (!books.length) {
+    return null
+  }
+
+  return books.find(({ id }) => id === activeBookId)
+})
+
+//* Events
+export const activeBookChanged = createEvent<number | null>()
+
+//* Effects
+export const pushBookFx = createEffect(({ book, books }: { book: Book; books: Book[] }) => {
+  const bookIndex = books.findIndex(({ id }) => id === book.id)
+
+  if (bookIndex === -1) {
+    return [...books, book]
+  }
+
+  books.splice(bookIndex, 1, book)
+
+  return books
+})
+
+//* Bussines Logic
+$activeBookId.on(activeBookChanged, (_, id) => id)
